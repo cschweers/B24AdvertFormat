c7ee8301044b627452b2098e31da3a8f6c939315
Christoph Schweers <christoph.schweers@gmx.net>
2023-12-21 22:10:59 +0100

    enhance app output
    Signed-off-by: Christoph Schweers <christoph.schweers@gmx.net>

--- B24AdvertFormat/Program.cs
+++ B24AdvertFormat/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,7 @@ namespace B24AdvertFormat
             if (parser8742.SetPayload(B24AdvertismentParser.DataType.Encoded, data1))
             {
                 B24Advertisment advert1 = parser8742.Advertisment;
+                printRawData(data1);
                 printAdvertisment(advert1);
             }
             else
@@ -28,6 +30,7 @@ namespace B24AdvertFormat
             if (parser0000.SetPayload(B24AdvertismentParser.DataType.Encoded, data2))
             {
                 B24Advertisment advert2 = parser0000.Advertisment;
+                printRawData(data2);
                 printAdvertisment(advert2);
             }
             else
@@ -37,13 +40,23 @@ namespace B24AdvertFormat
             if (parser0000.SetPayload(B24AdvertismentParser.DataType.Encoded, data3))
             {
                 B24Advertisment advert3 = parser0000.Advertisment;
+                printRawData(data3);
                 printAdvertisment(advert3);
             }
             else
             {
                 Console.WriteLine("Payload not set");
             }
-            Console.ReadKey();
+            if(Debugger.IsAttached)
+            {
+                Console.WriteLine("\npress any key to continue ...");
+                Console.ReadKey();
+            }
+        }
+
+        private static void printRawData(byte[] data)
+        {
+            Console.WriteLine("\nPayload raw data: {0}", BitConverter.ToString(data));
         }
 
         private static void printAdvertisment(B24Advertisment advert)
@@ -55,9 +68,18 @@ namespace B24AdvertFormat
             Console.WriteLine("ModuleID: {0}", BitConverter.ToString(advert.ModuleID));
             Console.WriteLine("Status: {0}", advert.Status);
             Console.WriteLine("Units: {0}", advert.Units);
-            Console.WriteLine("Data: {0}", BitConverter.ToString(advert.Data));
+            Console.WriteLine("Data: {0} ({1})", BitConverter.ToString(advert.Data), getFloatIeee754(advert.Data));
             Console.WriteLine("DataTag1: {0}", BitConverter.ToString(advert.DataTag1));
             Console.WriteLine("DataTag2: {0}", BitConverter.ToString(advert.DataTag2));
+        }
+
+        private static float getFloatIeee754(byte[] data)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(data); // We have to reverse
+            }
+            return BitConverter.ToSingle(data, 0);
         }
     }
 }
